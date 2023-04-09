aa777b5f14548178982bdc1b6eb09c6137fdb134
Steven Atkinson <steven@atkinson.mn>
2023-04-08 22:17:26 -0700

    Allocate activations on the stack
    Squashed commit of the following:
    
    commit cf01416ebb7bdd9e64719e84cac8fb74abd1a7b8
    Merge: 2363c1b 3a139b9
    Author: Steven Atkinson <steven@atkinson.mn>
    Date:   Sat Apr 8 22:16:52 2023 -0700
    
        Merge branch 'main' into mleak
    
    commit 2363c1b3aa96dad4e04b8c4468fd41f2bba73592
    Author: Steven Atkinson <steven@atkinson.mn>
    Date:   Sat Apr 8 22:15:31 2023 -0700
    
        Allocate activations on the stack
    
    commit 7288f0dea764e264f41424dd48399a06f5e908ea
    Merge: 482b69a 8d956a2
    Author: Steven Atkinson <steven@atkinson.mn>
    Date:   Sat Apr 8 19:02:11 2023 -0700
    
        Merge branch 'main' into activation_refactor
    
    commit 482b69aa547b72aedf0ac2569218111becb3d831
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Tue Apr 4 07:50:06 2023 -0700
    
        No need for middleCols() in full matrix apply anymore
    
    commit b41d172910024ee7eb179731b50615424af182d6
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 17:01:52 2023 -0700
    
        Fixed WaveNet sigmoid
    
    commit 26a725db8e8ab3e1ec1f505efc95b3ef8a5dfe0a
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 16:36:34 2023 -0700
    
        Use new activation code for WaveNet Sigmoid
    
    commit 74b0a4977917b67e54abb000223296e9e666595f
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 16:32:15 2023 -0700
    
        Simplify Activation apply
    
    commit 1cf8a4ff199b520dccf15487e2e632eec86cb3aa
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 14:47:31 2023 -0700
    
        Move static function implementations into .cpp
    
    commit a3f3ce7028c0fa02fb040b381a2fc21bb3e0088f
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 14:29:33 2023 -0700
    
        More switching to new activation implementaion
    
    commit 069679d450e10fd2f91af542dda629abf94b59d6
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 13:57:52 2023 -0700
    
        Fixed apply overrides
    
    commit 2aa7020fc1192837654b6a9c873e2b13159bcf52
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 13:35:00 2023 -0700
    
        Remove #pragma
    
    commit 14cb71e0b54f93c34796bda77372f5b2d8496f80
    Author: Mike Oliphant <oliphant@nostatic.org>
    Date:   Mon Apr 3 13:34:12 2023 -0700
    
        Begin to refactor activation function handling

--- NAM/activations.cpp
+++ NAM/activations.cpp
@@ -1,11 +1,17 @@
 #include "activations.h"
 
+activations::ActivationTanh _TANH = activations::ActivationTanh();
+activations::ActivationFastTanh _FAST_TANH = activations::ActivationFastTanh();
+activations::ActivationHardTanh _HARD_TANH = activations::ActivationHardTanh();
+activations::ActivationReLU _RELU = activations::ActivationReLU();
+activations::ActivationSigmoid _SIGMOID = activations::ActivationSigmoid();
+
 std::unordered_map<std::string, activations::Activation*> activations::Activation::_activations = {
-  {"Tanh", new activations::ActivationTanh()},
-  {"Hardtanh", new activations::ActivationHardTanh()},
-  {"Fasttanh", new activations::ActivationFastTanh()},
-  {"ReLU", new activations::ActivationReLU()},
-  {"Sigmoid", new activations::ActivationSigmoid()}};
+  {"Tanh", &_TANH},
+  {"Hardtanh", &_HARD_TANH},
+  {"Fasttanh", &_FAST_TANH},
+  {"ReLU", &_RELU},
+  {"Sigmoid", &_SIGMOID}};
 
 activations::Activation* tanh_bak = nullptr;
 
