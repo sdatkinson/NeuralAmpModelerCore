572f96ab32424f0d136f95d5ee9249889c5cb779
Mike Oliphant <oliphant@nostatic.org>
2023-04-17 12:04:09 -0700

    Added CMake config

---
+++ tools/loadmodel.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "NAM/dsp.h"
+
+int main(int argc, char* argv[])
+{
+  if (argc > 1)
+  {
+    char* modelPath = argv[1];
+
+    fprintf(stderr, "Loading model [%s]\n", modelPath);
+
+    auto model = get_dsp(modelPath);
+
+    if (model != nullptr)
+    {
+      fprintf(stderr, "Model loaded successfully\n");
+    }
+    else
+    {
+      fprintf(stderr, "Failed to load model\n");
+
+      exit(1);
+    }
+  }
+  else
+  {
+    fprintf(stderr, "Usage: loadmodel <model_path>\n");
+  }
+
+  exit(0);
+}
