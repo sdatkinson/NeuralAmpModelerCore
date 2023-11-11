95aa8c3d8500542d4ca0cba03c7b0660fd246b75
Steven Atkinson <steven@atkinson.mn>
2023-11-11 12:23:59 -0800

    Fix the build

--- NAM/version.h
+++ NAM/version.h
@@ -1,6 +1,7 @@
 #ifndef version_h
 #define version_h
 
+// Make sure this matches NAM version in ../CMakeLists.txt!
 #define NEURAL_AMP_MODELER_DSP_VERSION_MAJOR 0
 #define NEURAL_AMP_MODELER_DSP_VERSION_MINOR 0
 #define NEURAL_AMP_MODELER_DSP_VERSION_PATCH 1
