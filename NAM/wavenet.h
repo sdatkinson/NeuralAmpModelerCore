eecad2b4de6403ddb1a724eb0048839ffd316ed2
Oli Larkin <olilarkin@googlemail.com>
2024-01-20 00:49:54 +0100

    _layer_buffers

--- NAM/wavenet.h
+++ NAM/wavenet.h
@@ -122,14 +122,14 @@ private:
   // Buffers in between layers.
   // buffer [i] is the input to layer [i].
   // the last layer outputs to a short array provided by outside.
-  std::vector<Eigen::MatrixXf> _layer_buffers;
+  std::vector<Eigen::MatrixXf> mLayerBuffers;
   // The layer objects
   std::vector<_Layer> _layers;
 
   // Rechannel for the head
   Conv1x1 _head_rechannel;
 
-  long GetBufferSize() const { return this->_layer_buffers.size() > 0 ? this->_layer_buffers[0].cols() : 0; };
+  long GetBufferSize() const { return this->mLayerBuffers.size() > 0 ? this->mLayerBuffers[0].cols() : 0; };
   long GetChannels() const;
   // "One-indexed" receptive field
   // TODO remove!
