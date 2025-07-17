be87bdaa55def78a7cf59916d34600764a57a711
woodybury <whshortridge@gmail.com>
2025-07-17 10:22:38 -0400

    fix skeleton

--- ui/src/components/Player/PlayerSkeleton.tsx
+++ ui/src/components/Player/PlayerSkeleton.tsx
@@ -1,65 +1,129 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PREVIEW_MODE } from '../../types';
 import { LogoSm } from '../ui/LogoSm';
+import { Play } from '../ui/Play';
+import { Skip } from '../ui/Skip';
+import { Select } from '../ui/Select';
+import { ToggleSimple } from '../ui/ToggleSimple';
 
 interface PlayerSkeletonProps {
   previewMode: PREVIEW_MODE;
 }
 
 export const PlayerSkeleton: React.FC<PlayerSkeletonProps> = ({ previewMode }) => {
+  const visualizerRef = useRef<HTMLCanvasElement>(null);
+  const canvasWrapperRef = useRef<HTMLDivElement>(null);
+
+  const renderModelSelect = () => (
+    <Select
+      options={[]}
+      label='Model'
+      onChange={() => {}}
+      defaultOption={''}
+      disabled={true}
+    />
+  );
+
+  const renderIrSelect = () => (
+    <Select
+      options={[]}
+      label='IR'
+      onChange={() => {}}
+      defaultOption={''}
+      disabled={true}
+    />
+  );
+
   return (
-    <div className='bg-zinc-900 border border-zinc-700 text-white p-4 lg:p-8 pt-0 lg:pt-2 rounded-xl w-full opacity-50 flex flex-col gap-6'>
+    <div className='bg-zinc-900 border border-zinc-700 text-white p-4 lg:p-8 pt-0 lg:pt-2 rounded-xl w-full flex flex-col gap-6 opacity-50 cursor-not-allowed'>
+      {/* Player Controls */}
       <div className='flex items-center gap-4 overflow-hidden'>
-        <button className='p-0 focus:outline-none cursor-not-allowed' disabled>
-          <LogoSm />
-        </button>
         <button
-          className='p-0 focus:outline-none cursor-not-allowed opacity-60'
-          disabled
+          className='p-0 focus:outline-none'
+          aria-label={'Play'}
+          disabled={true}
         >
-          <span className='text-xs text-zinc-400'>v1.0.23</span>
+           <Play />
         </button>
+
+        <button
+          className='p-0 focus:outline-none'
+          aria-label='Skip to start'
+          disabled={true}
+        >
+          <Skip opacity={ 0.6} />
+        </button>
+
+        <div className='flex text-sm font-mono gap-2 text-zinc-400'>
+          <span>00:00</span>
+            <span> / </span>
+          <span>00:00</span>
+        </div>
+
+        <div ref={canvasWrapperRef} className='flex-1'>
+          <canvas
+            ref={visualizerRef}
+            height={130}
+            style={{
+              marginBottom: -20,
+              marginTop: -20,
+              width: '100%',
+              height: '130px',
+            }}
+          />
+        </div>
       </div>
 
-      <div className='flex text-sm font-mono gap-2 text-zinc-400'>
-        <span>Loading...</span>
-      </div>
-
-      <div className='flex-1'>
-        <canvas
-          className='w-full h-[130px] bg-zinc-900 rounded'
-          width={800}
-          height={130}
-        />
-      </div>
-
+      {/* Settings */}
       <div className='flex flex-col gap-2'>
         <div className='flex flex-row items-center gap-4 flex-wrap'>
           <div className='flex-1 min-w-[0px]'>
-            <div className='flex w-full flex-1 min-w-[0px]'>
-              <div className='w-full h-8 bg-zinc-800 rounded animate-pulse'></div>
+            <div className={`flex w-full flex-1 min-w-[0px]`}>
+              {previewMode === PREVIEW_MODE.MODEL
+                ? renderModelSelect()
+                : renderIrSelect()}
             </div>
+          </div>
+
+          <div className='flex items-center pt-[24px] flex-shrink-0'>
+            <ToggleSimple
+              label=''
+              onChange={() => {}}
+              isChecked={true}
+              ariaLabel='Bypass'
+              disabled={true}
+            />
+          </div>
+        </div>
+
+        <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-6'>
+          <div className='w-full sm:w-1/2'>
+            <Select
+              options={[]}
+              label='Input'
+              onChange={() => {}}
+              defaultOption={''}
+              disabled={true}
+            />
+          </div>
+
+          <div className={`w-full sm:w-1/2`}>
+            {previewMode === PREVIEW_MODE.MODEL
+              ? renderIrSelect()
+              : renderModelSelect()}
           </div>
         </div>
       </div>
 
-      <div className='flex items-center pt-[24px] flex-shrink-0'>
-        <div className='w-full h-8 bg-zinc-800 rounded animate-pulse'></div>
-      </div>
-
-      <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-6'>
-        <div className='w-full sm:w-1/2'>
-          <div className='w-full h-8 bg-zinc-800 rounded animate-pulse'></div>
-        </div>
-        <div className='w-full sm:w-1/2'>
-          <div className='w-full h-8 bg-zinc-800 rounded animate-pulse'></div>
-        </div>
-      </div>
-
-      <div className='flex flex-row gap-2 items-center self-end'>
-        <LogoSm />
+      {/* Footer */}
+      <a
+        href='https://www.tone3000.com'
+        target='_blank'
+        className='flex flex-row gap-2 items-center self-end'
+      >
         <p className='text-zinc-400 text-xs'>Powered by</p>
-      </div>
+        <LogoSm width={42} height={14} />
+      </a>
     </div>
   );
 };
