d1bfbfc1601abb4b40b4d3678fcc2a2a61b4f76c
Arunav Sanyal <arunav.sanyal91@gmail.com>
2025-11-29 21:27:58 +0000

    Migrate to using catch2 as test framework

--- tools/test/test_activations.cpp
+++ tools/test/test_activations.cpp
@@ -1,122 +1,89 @@
-// Tests for activation functions
-//
-// Things you want ot test for:
-// 1. That the core elementwise funciton is snapshot-correct.
-// 2. The class that wraps the core function for an array of data
-// 3. .cpp: that you have the singleton defined, and that it's in the unordered map to get by string
-
-#include <cassert>
 #include <string>
 #include <vector>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include "NAM/activations.h"
 
-namespace test_activations
+TEST_CASE("Test fast_tanh core function", "[activations]")
 {
-// TODO get nonzero cases
-class TestFastTanh
+  REQUIRE(nam::activations::fast_tanh(0.0f) == 0.0f);
+  // The original test had these commented out.
+  // REQUIRE(nam::activations::fast_tanh(1.0f) == Catch::Approx(0.98f));
+  // REQUIRE(nam::activations::fast_tanh(-1.0f) == Catch::Approx(-0.98f));
+}
+
+void _test_fast_tanh_class(nam::activations::Activation* a)
 {
-public:
-  static void test_core_function()
-  {
-    auto TestCase = [](float input, float expectedOutput) {
-      float actualOutput = nam::activations::fast_tanh(input);
-      assert(actualOutput == expectedOutput);
-    };
-    // A few snapshot tests
-    TestCase(0.0f, 0.0f);
-    // TestCase(1.0f, 1.0f);
-    // TestCase(-1.0f, -0.01f);
-  };
+  std::vector<float> inputs, expectedOutputs;
 
-  static void test_get_by_init()
+  inputs.push_back(0.0f);
+  expectedOutputs.push_back(0.0f);
+
+  // inputs.push_back(1.0f);
+  // expectedOutputs.push_back(1.0f);
+
+  // inputs.push_back(-1.0f);
+  // expectedOutputs.push_back(-0.01f);
+
+  a->apply(inputs.data(), (long)inputs.size());
+  for (size_t i = 0; i < inputs.size(); ++i)
   {
-    auto a = nam::activations::ActivationLeakyReLU();
-    _test_class(&a);
+    REQUIRE(inputs[i] == Catch::Approx(expectedOutputs[i]));
   }
+}
 
-  // Get the singleton and test it
-  static void test_get_by_str()
-  {
-    const std::string name = "Fasttanh";
-    auto a = nam::activations::Activation::get_activation(name);
-    _test_class(a);
-  }
-
-private:
-  // Put the class through its paces
-  static void _test_class(nam::activations::Activation* a)
-  {
-    std::vector<float> inputs, expectedOutputs;
-
-    inputs.push_back(0.0f);
-    expectedOutputs.push_back(0.0f);
-
-    // inputs.push_back(1.0f);
-    // expectedOutputs.push_back(1.0f);
-
-    // inputs.push_back(-1.0f);
-    // expectedOutputs.push_back(-0.01f);
-
-    a->apply(inputs.data(), (long)inputs.size());
-    for (auto itActual = inputs.begin(), itExpected = expectedOutputs.begin(); itActual != inputs.end();
-         ++itActual, ++itExpected)
-    {
-      assert(*itActual == *itExpected);
-    }
-  };
-};
-
-class TestLeakyReLU
+TEST_CASE("Test FastTanh get by init", "[activations]")
 {
-public:
-  static void test_core_function()
-  {
-    auto TestCase = [](float input, float expectedOutput) {
-      float actualOutput = nam::activations::leaky_relu(input);
-      assert(actualOutput == expectedOutput);
-    };
-    // A few snapshot tests
-    TestCase(0.0f, 0.0f);
-    TestCase(1.0f, 1.0f);
-    TestCase(-1.0f, -0.01f);
-  };
+  auto a = nam::activations::ActivationFastTanh();
+  _test_fast_tanh_class(&a);
+}
 
-  static void test_get_by_init()
+TEST_CASE("Test FastTanh get by str", "[activations]")
+{
+  const std::string name = "Fasttanh";
+  auto a = nam::activations::Activation::get_activation(name);
+  REQUIRE(a != nullptr);
+  _test_fast_tanh_class(a);
+}
+
+TEST_CASE("Test LeakyReLU core function", "[activations]")
+{
+  REQUIRE(nam::activations::leaky_relu(0.0f) == 0.0f);
+  REQUIRE(nam::activations::leaky_relu(1.0f) == 1.0f);
+  REQUIRE(nam::activations::leaky_relu(-1.0f) == -0.01f);
+}
+
+void _test_leaky_relu_class(nam::activations::Activation* a)
+{
+  std::vector<float> inputs, expectedOutputs;
+
+  inputs.push_back(0.0f);
+  expectedOutputs.push_back(0.0f);
+
+  inputs.push_back(1.0f);
+  expectedOutputs.push_back(1.0f);
+
+  inputs.push_back(-1.0f);
+  expectedOutputs.push_back(-0.01f);
+
+  a->apply(inputs.data(), (long)inputs.size());
+  for (size_t i = 0; i < inputs.size(); ++i)
   {
-    auto a = nam::activations::ActivationLeakyReLU();
-    _test_class(&a);
+    REQUIRE(inputs[i] == Catch::Approx(expectedOutputs[i]));
   }
+}
 
-  // Get the singleton and test it
-  static void test_get_by_str()
-  {
-    const std::string name = "LeakyReLU";
-    auto a = nam::activations::Activation::get_activation(name);
-    _test_class(a);
-  }
+TEST_CASE("Test LeakyReLU get by init", "[activations]")
+{
+  auto a = nam::activations::ActivationLeakyReLU();
+  _test_leaky_relu_class(&a);
+}
 
-private:
-  // Put the class through its paces
-  static void _test_class(nam::activations::Activation* a)
-  {
-    std::vector<float> inputs, expectedOutputs;
-
-    inputs.push_back(0.0f);
-    expectedOutputs.push_back(0.0f);
-
-    inputs.push_back(1.0f);
-    expectedOutputs.push_back(1.0f);
-
-    inputs.push_back(-1.0f);
-    expectedOutputs.push_back(-0.01f);
-
-    a->apply(inputs.data(), (long)inputs.size());
-    for (auto itActual = inputs.begin(), itExpected = expectedOutputs.begin(); itActual != inputs.end();
-         ++itActual, ++itExpected)
-    {
-      assert(*itActual == *itExpected);
-    }
-  };
-};
-}; // namespace test_activations
+TEST_CASE("Test LeakyReLU get by str", "[activations]")
+{
+  const std::string name = "LeakyReLU";
+  auto a = nam::activations::Activation::get_activation(name);
+  REQUIRE(a != nullptr);
+  _test_leaky_relu_class(a);
+}
--- tools/test/test_get_dsp.cpp
+++ tools/test/test_get_dsp.cpp
@@ -1,14 +1,12 @@
-#include <cassert>
 #include <memory>
 #include <string>
 #include <vector>
+#include <catch2/catch_test_macros.hpp>
 
 #include "json.hpp"
 
 #include "NAM/get_dsp.h"
 
-namespace test_get_dsp
-{
 // Config
 const std::string basicConfigStr =
   R"({"version": "0.5.4", "metadata": {"date": {"year": 2024, "month": 10, "day": 9, "hour": 18, "minute": 44, "second": 41}, "loudness": -37.8406867980957, "gain": 0.13508800804658277, "name": "Test LSTM", "modeled_by": "Steve", "gear_type": "amp", "gear_make": "Darkglass Electronics", "gear_model": "Microtubes 900 v2", "tone_type": "clean", "input_level_dbu": 18.3, "output_level_dbu": 12.3, "training": {"settings": {"ignore_checks": false}, "data": {"latency": {"manual": null, "calibration": {"algorithm_version": 1, "delays": [-16], "safety_factor": 1, "recommended": -17, "warnings": {"matches_lookahead": false, "disagreement_too_high": false}}}, "checks": {"version": 3, "passed": true}}, "validation_esr": null}}, "architecture": "LSTM", "config": {"input_size": 1, "hidden_size": 3, "num_layers": 1}, "weights": [-0.21677088737487793, -0.6683622002601624, -0.2560940980911255, -0.3588429093360901, 0.17952610552310944, 0.19445613026618958, -0.01662646047770977, 0.5353694558143616, -0.2536540627479553, -0.5132213234901428, -0.020476307719945908, 0.08592455089092255, -0.6891753673553467, 0.3627359867095947, 0.008421811275184155, 0.3113192617893219, 0.14251480996608734, 0.07989779114723206, -0.18211324512958527, 0.7118963003158569, 0.41084015369415283, -0.6571938395500183, -0.13214066624641418, -0.2698603868484497, 0.49387243390083313, -0.3491725027561188, 0.6353667974472046, -0.5005152225494385, 0.2052856683731079, -0.4301638901233673, -0.15770092606544495, -0.7181791067123413, 0.056290093809366226, -0.49049463868141174, 0.6623441576957703, 0.09029324352741241, 0.34005245566368103, 0.16416560113430023, 0.15520110726356506, -0.4155678153038025, -0.36928507685661316, 0.3211132884025574, -0.6769840121269226, -0.1575538069009781, 0.05268515646457672, -0.4191459119319916, 0.599330484867096, 0.21518059074878693, -4.246325492858887, -3.315647840499878, -4.328850746154785, 4.496089458465576, 5.015639305114746, 3.6492037773132324, 0.14431169629096985, -0.6633821725845337, 0.11673200130462646, -0.1418764889240265, -0.4897872805595398, -0.8689419031143188, -0.06714004278182983, -0.4450395107269287, -0.02142983116209507, -0.15136894583702087, -2.775207996368408, -0.08681213855743408, 0.05702732503414154, 0.670292317867279, 0.31442636251449585, 0.30793967843055725], "sample_rate": 48000})";
@@ -19,7 +17,7 @@ std::vector<float> GetWeights(nlohmann::json const& j)
   auto it = j.find("weights");
   if (it != j.end())
   {
-    return *it;
+    return it->get<std::vector<float>>();
   }
   else
   {
@@ -42,20 +40,21 @@ nam::dspData _GetConfig(const std::string& configStr = basicConfigStr)
   return returnedConfig;
 }
 
-void test_gets_input_level()
+TEST_CASE("get_dsp gets input level", "[get_dsp]")
 {
   nam::dspData config = _GetConfig();
   std::unique_ptr<nam::DSP> dsp = get_dsp(config);
-  assert(dsp->HasInputLevel());
-}
-void test_gets_output_level()
-{
-  nam::dspData config = _GetConfig();
-  std::unique_ptr<nam::DSP> dsp = get_dsp(config);
-  assert(dsp->HasOutputLevel());
+  REQUIRE(dsp->HasInputLevel());
 }
 
-void test_null_input_level()
+TEST_CASE("get_dsp gets output level", "[get_dsp]")
+{
+  nam::dspData config = _GetConfig();
+  std::unique_ptr<nam::DSP> dsp = get_dsp(config);
+  REQUIRE(dsp->HasOutputLevel());
+}
+
+TEST_CASE("get_dsp handles null input level", "[get_dsp]")
 {
   // Issue 129
   const std::string configStr =
@@ -64,11 +63,11 @@ void test_null_input_level()
   // The first part of this is that the following line doesn't fail:
   std::unique_ptr<nam::DSP> dsp = get_dsp(config);
 
-  assert(!dsp->HasInputLevel());
-  assert(dsp->HasOutputLevel());
+  REQUIRE(!dsp->HasInputLevel());
+  REQUIRE(dsp->HasOutputLevel());
 }
 
-void test_null_output_level()
+TEST_CASE("get_dsp handles null output level", "[get_dsp]")
 {
   // Issue 129
   const std::string configStr =
@@ -76,7 +75,6 @@ void test_null_output_level()
   nam::dspData config = _GetConfig(configStr);
   // The first part of this is that the following line doesn't fail:
   std::unique_ptr<nam::DSP> dsp = get_dsp(config);
-  assert(dsp->HasInputLevel());
-  assert(!dsp->HasOutputLevel());
+  REQUIRE(dsp->HasInputLevel());
+  REQUIRE(!dsp->HasOutputLevel());
 }
-}; // namespace test_get_dsp
